2ec33cfde23912493ae10de95e256c7dc4aca9da
Emre iş yeri <sjoxlyses7@gmail.com>
2026-03-13 16:51:48 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        data: {
+          title: 'Anasayfa | Suv Market',
+          description: 'Suv Market İkinci El güvenilir ikinci el araç alım ve satım hizmeti sunar. Ekspertizli, garantili ve kaliteli araçları güvenle keşfedin.'
+        },
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+      }
+]
+}
+]
